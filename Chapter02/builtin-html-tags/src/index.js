fb9eab452966ebbcd0e6daefeb272a58c9d76d23
Walisson Rodrigues <contact@walisson.com>
2020-09-20 14:06:04 +1000

    Chapter 02 - Builtint-html-tags - Exercise completed

---
+++ Chapter02/builtin-html-tags/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { render } from "react-dom";
+
+render(
+  <div>
+    <button></button>
+    <code></code>
+    <input />
+    <label></label>
+    <p></p>
+    <pre></pre>
+    <select></select>
+    <table></table>
+    <ul></ul>
+  </div>,
+  document.getElementById("root")
+);
