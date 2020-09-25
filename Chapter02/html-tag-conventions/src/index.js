7192445f93f505764d7b94e42d9f57bfbc54c30f
Walisson Rodrigues <contact@walisson.com>
2020-09-25 17:29:47 +1000

    Chapter 02 - Html-tag-conventions - Exercise completed

---
+++ Chapter02/html-tag-conventions/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "react-dom";
+
+render(
+  <button title="My Button" foo="bar">
+    My Button
+  </button>,
+  document.getElementById("root")
+);
