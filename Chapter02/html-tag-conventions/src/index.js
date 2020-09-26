99fe9dbe315d3265c731d07319e667ace0adbe86
Walisson Rodrigues <contact@walisson.com>
2020-09-26 18:07:58 +1000

    Chapter 02 - Html-tag-conventions - Enhancement implementation

--- Chapter02/html-tag-conventions/src/index.js
+++ Chapter02/html-tag-conventions/src/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
+import "./index.css";
+import CommandIcon from "./img/command-icon.svg";
 
 render(
-  <button title="My Button" foo="bar">
-    My Button
+  <button title="My Button" foo="bar" className="button">
+    <div className="button-content">
+      <div className="button-icon">
+        <img src={CommandIcon} alt="Command key icon" />
+      </div>
+      <p className="button-text">command</p>
+    </div>
   </button>,
   document.getElementById("root")
 );
