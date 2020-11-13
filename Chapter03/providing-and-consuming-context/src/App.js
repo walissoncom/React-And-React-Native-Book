d53e82100ad99a70c5410deb9158aa600a8c8b40
Walisson Rodrigues <contact@walisson.com>
2020-11-13 11:59:33 +1100

    Chapter03 - Providing-and-consuming-context - Exercise completed

---
+++ Chapter03/providing-and-consuming-context/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import First from './First';
+import Second from './Second';
+import Third from './Third';
+
+export default () => (
+  <>
+    <First />
+    <Second />
+    <Third />
+  </>
+)
